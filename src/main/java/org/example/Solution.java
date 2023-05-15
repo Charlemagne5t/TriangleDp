b67377e30036924a984c48febadf3dde1224447a
alek <wait82985@gmail.com>
2023-05-15 22:15:48 +0600

    Problem solved

--- src/main/java/org/example/Solution.java
+++ src/main/java/org/example/Solution.java
@@ -4,7 +4,20 @@ import java.util.List;
 
 public class Solution {
     public int minimumTotal(List<List<Integer>> triangle) {
+        int sizeOfLastRow = triangle.get(triangle.size() - 1).size();
+        int[][] dp = new int[triangle.size()][sizeOfLastRow];
+        //base case
+        for (int i = 0; i < sizeOfLastRow; i++) {
+            dp[triangle.size() - 1][i] = triangle.get(triangle.size() - 1).get(i);
+        }
 
-        return 1;
+        for (int i = triangle.size() - 2; i >= 0; i--) {
+            for (int j = 0; j < triangle.get(i).size(); j++) {
+
+                dp[i][j] = Math.min(dp[i + 1][j], dp[i + 1][j + 1]) + triangle.get(i).get(j);
+            }
+        }
+        return dp[0][0];
     }
 }
+
