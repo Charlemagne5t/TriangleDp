f688a6543f508f453e79131750c86e82d2866f31
alek <wait82985@gmail.com>
2023-05-15 22:17:25 +0600

    add comments

--- src/main/java/org/example/Solution.java
+++ src/main/java/org/example/Solution.java
@@ -10,13 +10,14 @@ public class Solution {
         for (int i = 0; i < sizeOfLastRow; i++) {
             dp[triangle.size() - 1][i] = triangle.get(triangle.size() - 1).get(i);
         }
-
+        //building dp table bottom -> up
         for (int i = triangle.size() - 2; i >= 0; i--) {
             for (int j = 0; j < triangle.get(i).size(); j++) {
 
                 dp[i][j] = Math.min(dp[i + 1][j], dp[i + 1][j + 1]) + triangle.get(i).get(j);
             }
         }
+        //result will be in dp[0][0]
         return dp[0][0];
     }
 }
